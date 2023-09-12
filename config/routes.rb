c04d034eda1e891923053369fcb531d1be3ed758
shakar hassan <shakarmohammed2002@gmail.com>
2023-09-12 13:19:55 +0200

    needs a littel bit more

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "tasks/new", to: "tasks#new", as: :new
   post "tasks", to: "tasks#create"
   get "tasks/:id", to: "tasks#show", as: :task
-  get "tasks/:id/edit", to: "tasks#edit"
-  patch "tasks/:id", to: "tasks#update", as: :update
-  delete "tasks/id", to: "tasks#destroy"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
